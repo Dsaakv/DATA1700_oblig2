f7bcf4a7cddc58a1c351234a4032a4782f85ca5a
Your Name <you@example.com>
2024-03-04 09:32:07 +0100

    Fikset lagring og sletting av billetter på server side via get og post mapping

---
+++ src/main/resources/static/registrer.js
@@ -0,0 +1,85 @@
+function registrerbilett() {
+
+    const film = document.getElementById("film").value;
+    const antall = document.getElementById("antall").value;
+    const fornavn = document.getElementById("fornavn").value;
+    const etternavn = document.getElementById("etternavn").value;
+    const telefonnr = document.getElementById("telefonnr").value;
+    const epost = document.getElementById("epost").value;
+    const antallfeil = document.getElementById("feilmeldingantall");
+    const fornavnfeil = document.getElementById("feilmeldingfornavn");
+    const etternavnfeil = document.getElementById("feilmeldingetternavn");
+    const telefonnrfeil = document.getElementById("feilmeldingtelefonnr");
+    const epostfeil = document.getElementById("feilmeldingepost");
+
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let regexPhone = /^\d{8}$/;
+
+    filmfeil.innerHTML="";
+    antallfeil.innerHTML = '';
+    fornavnfeil.innerHTML = '';
+    etternavnfeil.innerHTML = '';
+    telefonnrfeil.innerHTML = '';
+    epostfeil.innerHTML = '';
+
+    let errors = [];
+
+
+    if (film === "") {
+        filmfeil.innerHTML="Du må velge en film";
+        errors.push('filmfeil');
+    }
+
+    if (antall.length === 0){
+        antallfeil.innerHTML="Du må skrive noe inn i antall";
+        errors.push('antallfeil');
+    }
+
+
+    if (fornavn.length === 0){
+        fornavnfeil.innerHTML="Du må skrive noe inn i fornavn";
+        errors.push('fornavnfeil');
+    }
+
+
+    if (etternavn.length === 0){
+        etternavnfeil.innerHTML="Du må skrive noe inn i etternavn";
+        errors.push('etternavnfeil');
+    }
+
+
+    if (telefonnr.length === 0){
+        telefonnrfeil.innerHTML="Du må skrive noe inn i telefonnr";
+        errors.push('telefonnrfeil');
+    } else if (!regexPhone.test(telefonnr)) {
+        telefonnrfeil.innerHTML = "Du må skrive et gyldig telefonnummer (8 siffer)";
+        errors.push('telefonnrfeil');
+    }
+
+
+    if (epost.length === 0){
+        epostfeil.innerHTML="Du må skrive noe inn i epost";
+        errors.push('epostfeil');
+    } else if (!regex.test(epost)) {
+        epostfeil.innerHTML = "Du må skrive inn en gyldig epost";
+        errors.push('epostfeil');
+    }
+
+
+    if (errors.length > 0) {
+        return false;
+    }else {
+        const bilett = {
+            film: film,
+            antall: antall,
+            fornavn: fornavn,
+            etternavn: etternavn,
+            telefonnr: telefonnr,
+            epost: epost
+        };
+        $.post("/lagre", bilett, function(){
+            window.location.href= "index.html";
+        });
+        document.getElementById("bilettForm").reset();
+    }
+}
